d7ce6a014da67f18054c9eb309f2eb44b16bac50
Rahul Kumar <kumarrahul771996@gmail.com>
2023-08-20 20:03:04 +0000

    Fixed a bug in the sumit() function.

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ def sumit(till):
     '''A recursive fn to sum till n'''
     if till == 0:
         return 1
-    else:
-        return till + sumit(till - 1)
+    return till + sumit(till - 1)
 
 print(f"Sum till 5 using recursion: {sumit(5)}")
