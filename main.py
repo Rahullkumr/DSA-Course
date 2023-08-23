97f014ffb046f8556b534ad82c842f0d8e8ed0bc
Rahul Kumar <kumarrahul771996@gmail.com>
2023-08-23 10:37:08 +0000

    ``` This commit changes the code from a recursive function to a print statement. ```

--- main.py
+++ main.py
@@ -1,12 +1,1 @@
-'''
-    Docstring
-'''
-
-
-def sumit(till):
-    '''A recursive fn to sum till n'''
-    if till == 0:
-        return 1
-    return till + sumit(till - 1)
-
-print(f"Sum till 5 using recursion: {sumit(5)}")
+print('hello world')
