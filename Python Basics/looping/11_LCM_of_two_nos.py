5943fb923a356ae90edbdcaa6438ce9c2357626f
Rahul Kumar <kumarrahul771996@gmail.com>
2024-01-27 17:07:19 +0000

    vvi: add lcm and hcf of two numbers

--- Python Basics/looping/11_LCM_of_two_nos.py
+++ Python Basics/looping/11_LCM_of_two_nos.py
@@ -1,2 +1,26 @@
 # Calculate LCM of two given numbers using while loop
+# LCM * HCF = num1 * num2
 
+def LCM_byMe(num1, num2):
+    bigger = num1 if num1 > num2 else num2
+    i = 1
+    while True:
+        multiple = bigger * i
+        if multiple % num1 == 0 and multiple % num2 == 0:
+            print(f'LCM by me = {multiple}')
+            break
+        i += 1
+
+# LCM optimised wrt time complexity for INTERVIEWBIT
+def LCM_optimised(a, b):
+    def hcf(a, b):
+        while b:
+            a, b = b, a % b
+        return a
+
+    print(f'Optimized LCM = {(a * b) // hcf(a, b)}')
+
+num1, num2 = int(input("Enter first number: ")), int(input("Enter second number: "))
+
+LCM_byMe(num1, num2)
+LCM_optimised(num1, num2)
