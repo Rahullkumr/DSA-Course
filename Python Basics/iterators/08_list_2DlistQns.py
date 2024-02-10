a43f687bf3c4e0ebe86494525fbb4b74771d8368
Rahul Kumar <kumarrahul771996@gmail.com>
2024-02-10 17:51:44 +0000

    chore(add): 2D list print

--- Python Basics/iterators/08_list_2DlistQns.py
+++ Python Basics/iterators/08_list_2DlistQns.py
@@ -1,3 +1,14 @@
+# 2D array / list 
+
+arr = [[e for e in range(3)],[2*e for e in range(3)],[2*e-1 for e in range(1,4)]]
+# [[0, 1, 2], [0, 2, 4], [1, 3, 5]]
+
+for i in range(3):
+    for j in range(3):
+        print(arr[i][j], end=" ")
+    print()
+
 # search an element in a 2D list
 # sum and average of all elements in a 2D list
 # find sum of both diagonal elements in a 2D list
+
